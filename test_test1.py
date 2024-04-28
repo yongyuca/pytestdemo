5da4f098289a904b83c1d88e9267aba53b3b1fdb
yong yu <yongyuit@gmail.com>
2024-04-28 15:34:48 -0400

    Update test_test1.py

--- test_test1.py
+++ test_test1.py
@@ -8,6 +8,6 @@ def test_some_primes():
     assert 37 in {
         num
         for num in range(2, 50)
-        echo "num"
+        echo "$num"
         if not any(num % div == 0 for div in range(2, num))
     }
